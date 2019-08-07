e0f9b2ed13e849961f3999c85c9313a1a3cdcd2e
infernolisarum <infernolisarum@gmail.com>
2019-08-07 17:28:01 +0300

    Initial commit

---
+++ src/IOAlgorithms.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IOAlgorithms {
+
+    private static String testValueName = "Вячеслав";
+    private static List <Object> resultArray = new ArrayList <> ();
+    private static int[] testIntArray = {1, 20, 30, 96, 13, 9, 26, 90, 3, 0};
+
+    public static void main(String[] args) {
+    }
+
+    /*
+    Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+     */
+    static void firstExercise() {
+        Scanner input = new Scanner (System.in);
+        if (input.hasNextDouble () && input.nextDouble () > 7) System.out.println ("Привет");
+    }
+
+    /*
+    Составить алгоритм: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”,
+    если нет, то вывести "Нет такого имени"
+     */
+    static void secondExercise() {
+        Scanner input = new Scanner (System.in);
+        if (input.hasNext () && input.next ().equals (testValueName)) System.out.println ("Привет, " + testValueName);
+        else System.out.println ("Нет такого имени");
+    }
+
+    /*
+    Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
+     */
+    static void thirdExercise(int[] ints) {
+        try {
+            for (int i : ints) {
+                if (i % 3 == 0) resultArray.add (i);
+            }
+        } catch (NullPointerException e) {
+            System.out.println ("The passed array is empty");
+        }
+        try {
+            System.out.println (resultArray);
+        } catch (NullPointerException e) {
+            System.out.println ("The result array is empty");
+        }
+    }
+
+    static void thirdExercise(double[] ints) {
+        try {
+            for (double d : ints) {
+                if (d % 3 == 0) resultArray.add (d);
+            }
+        } catch (NullPointerException e) {
+            System.out.println ("The passed array is empty");
+        }
+        try {
+            System.out.println (resultArray);
+        } catch (NullPointerException e) {
+            System.out.println ("The result array is empty");
+        }
+    }
+}
